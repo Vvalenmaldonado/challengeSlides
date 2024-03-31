9f19f879d2b09ee5d5068325106e3f05695e6ee8
Valentina <vvalenmaldonado@gmail.com>
2024-03-31 15:42:53 -0300

    -

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -7,7 +7,6 @@ export function Button({ className, children, svg, ...props }: ButtonProps) {
   return (
     <button
       type="button"
-      id="getStarted"
       aria-label="Go to get started"
       className={clsx(
         "font-lato leading-[14px] text-[14px] flex flex-row items-center justify-center gap-[10px] bg-[#000000] text-white rounded-[10px] py-5 px-8 font-bold text-nowrap border-2 border-transparent hover:border-[#ddf247] hover:text-[#ddf247] transition-all duration-300",
