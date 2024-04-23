ce7be82d5df670ab3960bd8f9d1924fc185d7aff
Valentina <vvalenmaldonado@gmail.com>
2024-04-23 18:46:03 -0300

    changed name project and type App

--- app/app.tsx
+++ app/app.tsx
@@ -1,6 +1,7 @@
+import { AppProps } from "next/app";
 import RootLayout from "./layout";
 
-export default function App({ Component, pageProps }: any) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <RootLayout>
       <Component {...pageProps} />
